2b27c7d49f8a0371bbd86aafa5be5b5b3275ee7e
Vardan Hakobyan <vardan_live@live.com>
2024-09-02 13:35:44 +0400

    feat: even more TS-related updates (another attempt -2 )

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
+
+console.log('here, in index.js...');
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
