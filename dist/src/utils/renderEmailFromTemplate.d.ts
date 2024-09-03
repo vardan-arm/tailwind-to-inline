dc594dcc81bd4ac10f3b2989558a1a7bcd333eac
Vardan Hakobyan <vardan_live@live.com>
2024-09-03 15:29:05 +0400

    chore: resolve merge conflict

--- dist/src/utils/renderEmailFromTemplate.d.ts
+++
@@ -1,6 +0,0 @@
-type TRenderEmailFromTemplate = (template: string, placeholderValues?: {
-    [key: string]: string;
-}) => Promise<string>;
-export declare const renderEmailFromTemplate: TRenderEmailFromTemplate;
-export {};
-//# sourceMappingURL=renderEmailFromTemplate.d.ts.map
