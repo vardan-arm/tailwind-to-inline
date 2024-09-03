939e4f20f21a2d0e18e33071eee30b9cf2946127
Vardan Hakobyan <vardan_live@live.com>
2024-09-03 16:44:13 +0400

    test: run only *.ts files in tests, also check in tests that placeholders are correctly replaced by values

--- dist/src/utils/renderEmailFromTemplate.test.js
+++ dist/src/utils/renderEmailFromTemplate.test.js
@@ -11,15 +11,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const makeStylesInline_1 = require("./makeStylesInline");
 describe('renderEmailFromTemplate', () => {
-    const templatePath = 'src/example-template-minimal-with-html-tag.html';
+    const templatePath = 'src/mocks/example-template.html';
+
     test('should render email from template', () => __awaiter(void 0, void 0, void 0, function* () {
         const placeholderValues = {
             name: 'John Doe',
             thank_you: 'Thank you for signing up!',
-            cta_link: 'https://another.example.com',
+            cta_link: 'https://example.com',
             cta_text: 'See all features'
         };
-        const inlinedHtml = yield (0, makeStylesInline_1.makeStylesInline)(templatePath); // TODO: put placeholders...
+        const inlinedHtml = yield (0, makeStylesInline_1.makeStylesInline)(templatePath, placeholderValues);
+
         expect(inlinedHtml).toEqual(`<html>
   <head>
     <style>
@@ -31,7 +33,10 @@ describe('renderEmailFromTemplate', () => {
   </head>
   <body>
     <div class="pt-10 pl-4 max-w-[512px] relative z-20" style="position: relative; z-index: 20; max-width: 512px; padding-left: 1rem; padding-top: 2.5rem;">
-      <span class="mr-5" style="margin-right: 1.25rem;">Test</span>
+      <span class="mr-5" style="margin-right: 1.25rem;">Welcome, John Doe</span>
+    </div>
+    <div>
+      <a href="https://example.com" class="bg-blue-500 text-white rounded hover:bg-blue-600" style="border-radius: 0.25rem; --tw-bg-opacity: 1; background-color: rgb(59 130 246 / var(--tw-bg-opacity)); --tw-text-opacity: 1; color: rgb(255 255 255 / var(--tw-text-opacity));">See all features</a>
     </div>
   </body>
 </html>
