796051ec1c5b14cb2583fc9a2d285acd26fb6aec
Markos Mk <markosmk@me.com>
2026-03-09 09:24:34 -0300

    fix: keep the original string with the same indentations

--- src/utils/makeStylesInline.test.ts
+++ src/utils/makeStylesInline.test.ts
@@ -1,4 +1,7 @@
-import { makeStylesInline, makeStylesInlineFromString } from './makeStylesInline';
+import {
+  makeStylesInline,
+  makeStylesInlineFromString,
+} from './makeStylesInline';
 import * as fs from 'fs';
 
 describe('renderEmailFromTemplate', () => {
@@ -10,30 +13,32 @@ describe('renderEmailFromTemplate', () => {
     cta_text: 'See all features',
   };
   const expectedHtml = `<html>
-    <head>
-      <title>Test title</title>
-    </head>
-    <body>
-      <div style="position: relative; z-index: 20; max-width: 512px; padding-left: 1rem; padding-top: 2.5rem;">
-        <span style="margin-right: 1.25rem; color: #fde047;">Welcome, John Doe</span>
-      </div>
-      <div>
-        <a href="https://example.com" style="background-color: #3b82f6;">See all features</a>
-        <div style="background-image: url('https://example.com/custom-image.png'); background-repeat: no-repeat;"></div>
-      </div>
-    </body>
-  </html>`;
-  
-  const normalizeHtml = (html: string) => html.replace(/\s+/g, '').trim();
+  <head>
+    <title>Test title</title>
+  </head>
+  <body>
+    <div style="position: relative; z-index: 20; max-width: 512px; padding-left: 1rem; padding-top: 2.5rem;">
+      <span style="margin-right: 1.25rem; color: #fde047;">Welcome, John Doe</span>
+    </div>
+    <div>
+      <a href="https://example.com" style="background-color: #3b82f6;">See all features</a>
+      <div style="background-image: url('https://example.com/custom-image.png'); background-repeat: no-repeat;"></div>
+    </div>
+  </body>
+</html>
+`;
 
   test('should render email from template', async () => {
     const inlinedHtml = await makeStylesInline(templatePath, placeholderValues);
-    expect(normalizeHtml(inlinedHtml)).toEqual(normalizeHtml(expectedHtml));
+    expect(inlinedHtml).toEqual(expectedHtml);
   });
 
   test('should render email from raw template string', async () => {
     const rawString = fs.readFileSync(templatePath, 'utf8');
-    const inlinedHtml = await makeStylesInlineFromString(rawString, placeholderValues);
-    expect(normalizeHtml(inlinedHtml)).toEqual(normalizeHtml(expectedHtml));
+    const inlinedHtml = await makeStylesInlineFromString(
+      rawString,
+      placeholderValues,
+    );
+    expect(inlinedHtml).toEqual(expectedHtml);
   });
 });
