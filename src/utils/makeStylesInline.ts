333176ac019db1759f4313006cbaef086d8432c2
Markos Mk <markosmk@me.com>
2026-03-09 09:10:10 -0300

    chore: applied formatting correctly

--- src/utils/makeStylesInline.ts
+++ src/utils/makeStylesInline.ts
@@ -76,24 +76,27 @@ const inlineStyles = async (html: string): Promise<string> => {
 
 const processTemplate = async (
   templateSource: string,
-  data?: { [key: string]: string }
+  data?: { [key: string]: string },
 ): Promise<string> => {
-  const template = Handlebars.compile(templateSource)
-  const html = template(data)
+  const template = Handlebars.compile(templateSource);
+  const html = template(data);
 
-  const inlinedStyles = await inlineStyles(html)
+  const inlinedStyles = await inlineStyles(html);
 
-  return removeCssClasses(inlinedStyles)
-}
+  return removeCssClasses(inlinedStyles);
+};
 
-export const makeStylesInline: TMakeStylesInline = async (templatePath, data) => {
-  const templateSource = fs.readFileSync(templatePath, "utf8")
-  return processTemplate(templateSource, data)
-}
+export const makeStylesInline: TMakeStylesInline = async (
+  templatePath,
+  data,
+) => {
+  const templateSource = fs.readFileSync(templatePath, 'utf8');
+  return processTemplate(templateSource, data);
+};
 
 export const makeStylesInlineFromString = async (
   templateString: string,
-  data?: { [key: string]: string }
+  data?: { [key: string]: string },
 ): Promise<string> => {
-  return processTemplate(templateString, data)
-}
+  return processTemplate(templateString, data);
+};
